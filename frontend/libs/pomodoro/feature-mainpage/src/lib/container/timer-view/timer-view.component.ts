7caf262ad81180b4295b6229c898d714d355be6f
florian-vogel <fl_vogel@web.de>
2022-07-13 00:11:35 +0200

    move timer to shared

--- frontend/libs/pomodoro/feature-mainpage/src/lib/container/timer-view/timer-view.component.ts
+++ frontend/libs/pomodoro/feature-mainpage/src/lib/container/timer-view/timer-view.component.ts
@@ -5,7 +5,7 @@ import {
   TimerAction,
   TimerConfig,
   TimerState,
-} from '../../functions/createTimer';
+} from '../../../../../../shared/ui-common/src/lib/functions/createTimer';
 
 type Phase = 'work' | 'break' | 'longBreak';
 
